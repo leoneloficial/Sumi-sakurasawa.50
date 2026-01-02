782c9973d0d94457a1359536a095242955911ab6
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-02 19:17:41 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const config = {
   prefijo: '.',
 
   owner: [
-    '156981591593126@lid',
-    '50493732693@s.whatsapp.net',
-    '51921826291@s.whatsapp.net'
+    '393715279301',
+    '393715279301',
+    '393715279301'
   ],
 
   restrict: false
